b78d445d06f3fcb6bebe843eebb1b7640c1b930d
Riccardo Forina <riccardo.forina@prodigysgroup.it>
2012-11-16 16:20:32 +0100

    pypi ready

---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+import os
+
+CLASSIFIERS = [
+    'Development Status :: 3 - Alpha',
+    'Environment :: Web Environment',
+    'Framework :: Django',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: BSD License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    'Topic :: Software Development',
+    'Topic :: Software Development :: Libraries :: Application Frameworks',
+]
+
+setup(
+    author="Riccardo Forina",
+    author_email="riccardo.forina@codingnot.es",
+    name='django-admin-bootstrapped',
+    version='0.1',
+    description='A Bootstrap theme for Django Admin',
+    # long_description=open(os.path.join(os.path.dirname(__file__), 'README.md')).read(),
+    url='https://codingnot.es/',
+    license='BSD License',
+    platforms=['OS Independent'],
+    classifiers=CLASSIFIERS,
+    install_requires=[
+        'setuptools',
+        'Django>=1.4,<1.5',
+    ],
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False
+)
