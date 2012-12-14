cd68526bdf9eecf18683ce2a23e0f6c80c1f575c
Kyle Bock <kylewbock@gmail.com>
2012-12-14 09:56:48 -0500

    Refactor due to feedback.
    Created django_admin_bootstrapped.admin module for admin models. Created SortableInline mixin that adds the sortable javascript as wells as css for the sortables.
    
    usage invoked in admin.py as follows
    ```
    class MyInline(admin.TabularInline, SortableInline):
        # override sortable_field_name if it is not a position field on the model

---
+++ django_admin_bootstrapped/admin/models.py
@@ -0,0 +1,10 @@
+class SortableInline:
+    sortable_field_name = "position"
+    class Media:
+        js = (
+            '/static/admin/js/jquery.sortable.js',
+        )
+
+        css = {
+            'all':('/static/admin/css/admin-inlines.css',)
+        }
