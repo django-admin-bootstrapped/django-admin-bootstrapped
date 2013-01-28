7b9d56d470bd7400ff9d80720c86d333fc772145
Riccardo Forina <riccardo.forina@prodigysgroup.it>
2013-01-28 21:39:49 +0100

    partial fix for #29; actions must be declared inside the form to work: removed from the top and enabled on the bottom as default

--- test_django_admin_bootstrapped/test_django_admin_bootstrapped/admin.py
+++ test_django_admin_bootstrapped/test_django_admin_bootstrapped/admin.py
@@ -18,16 +18,18 @@ class TestSortable(admin.StackedInline, SortableInline):
 
 class TestMeAdmin(admin.ModelAdmin):
     list_display = ['test_ip', 'test_url', 'test_int', 'test_img', 'test_file', 'test_date', 'test_char', 'test_bool', 'test_time', 'test_slug', 'test_text', 'test_email', 'test_float', 'test_bigint', 'test_positive_integer', 'test_decimal', 'test_comma_separated_int', 'test_small_int', 'test_nullbool', 'test_filepath', 'test_positive_small_int', ]
-    list_search = ['test_url', ]
-    list_editable = ['test_url', ]
+    search_fields = ['test_int', ]
+    list_editable = ['test_int', ]
     list_filter = ['test_ip', 'test_url', 'test_int', ]
     list_per_page = 3
+    date_hierarchy = 'test_date'
     inlines = [TestThatStackedInline, TestThatTabularInline, TestSortable]
     save_as = True
     save_on_top = True
 
 
 class TestMeAdminFieldsets(TestMeAdmin):
+    actions_on_bottom = True
     fieldsets = (
         ('A fieldset', {
             'fields': ['test_m2m', 'test_ip', 'test_url', 'test_int', 'test_img', 'test_file', 'test_date', 'test_char', 'test_bool', 'test_time', 'test_slug', 'test_text', ],
