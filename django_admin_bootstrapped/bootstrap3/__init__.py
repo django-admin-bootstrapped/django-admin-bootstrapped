82d90e96464e55eed9004a2f9e99a1504abc64a7
Riccardo Magliocchetti <riccardo.magliocchetti@gmail.com>
2014-08-22 15:23:59 +0200

    bootstrap3: Add AppConfig for django 1.7
    Also use a label that don't conflict with django-bootstrap3
    
    Fix #124

---
+++ django_admin_bootstrapped/bootstrap3/__init__.py
@@ -0,0 +1,1 @@
+default_app_config = "django_admin_bootstrapped.bootstrap3.apps.Bootstrap3Config"
---
+++ django_admin_bootstrapped/bootstrap3/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+class Bootstrap3Config(AppConfig):
+    name = "django_admin_bootstrapped.bootstrap3"
+    label = "django_admin_bootstrapped_bootstrap3"
+    verbose_name = "Django Admin Bootstrapped Bootstrap 3"
