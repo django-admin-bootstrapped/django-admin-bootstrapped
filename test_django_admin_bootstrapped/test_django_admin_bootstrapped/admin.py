08315bb72279dfd738b78b23f623156a4d9950a0
Riccardo Magliocchetti <riccardo.magliocchetti@gmail.com>
2014-09-12 15:11:31 +0200

    test app: use collapsable fieldsets
    Both in ModelAdmin and stacked inlines.

--- test_django_admin_bootstrapped/test_django_admin_bootstrapped/admin.py
+++ test_django_admin_bootstrapped/test_django_admin_bootstrapped/admin.py
@@ -6,6 +6,14 @@ from .models import TestMe, TestThat, TestMeProxyForFieldsets, TestSortable
 class TestThatStackedInline(admin.StackedInline):
     model = TestThat
 
+class TestThatStackedFieldsetInline(admin.StackedInline):
+    model = TestThat
+    fieldsets = (
+        ('A collapsed fieldset', {
+            'classes': ('collapse',),
+            'fields': [('test_ip', 'test_url'), 'test_date',]
+        }),
+    )
 
 class TestThatTabularInline(admin.TabularInline):
     model = TestThat
@@ -30,13 +38,18 @@ class TestMeAdmin(admin.ModelAdmin):
 
 class TestMeAdminFieldsets(TestMeAdmin):
     actions_on_bottom = True
+    inlines= [TestThatStackedFieldsetInline]
     fieldsets = (
         ('A fieldset', {
             'fields': ['test_m2m', 'test_ip', 'test_url', 'test_int', 'test_img', 'test_file', 'test_date', 'test_char', 'test_bool', 'test_time', 'test_slug', 'test_text', ],
         }),
         ('Another fieldset', {
-            'fields': [('test_email', 'test_float'), 'test_bigint', ('test_positive_integer', 'test_decimal'), ('test_comma_separated_int', 'test_small_int'), ('test_nullbool', 'test_filepath'), 'test_positive_small_int', ],
+            'fields': [('test_email', 'test_float'), 'test_bigint', ('test_positive_integer', 'test_decimal'), ('test_comma_separated_int', 'test_small_int'),],
         }),
+        ('A collapsed fieldset', {
+            'classes': ('collapse',),
+            'fields': [('test_nullbool', 'test_filepath'), 'test_positive_small_int',]
+        })
     )
 
 admin.site.register(TestMeProxyForFieldsets, TestMeAdminFieldsets)
