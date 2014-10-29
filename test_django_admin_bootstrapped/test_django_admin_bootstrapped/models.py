a76865c3009e6f890b0cecc8d3e744ea22559d02
Riccardo Magliocchetti <riccardo.magliocchetti@gmail.com>
2014-10-29 17:31:56 +0100

    test project: add default values to BooleanFields
    To shut up Django 1.7 warnings

--- test_django_admin_bootstrapped/test_django_admin_bootstrapped/models.py
+++ test_django_admin_bootstrapped/test_django_admin_bootstrapped/models.py
@@ -10,7 +10,7 @@ class TestMe(models.Model):
     test_file = models.FileField(upload_to='dummy', blank=True)
     test_date = models.DateField(help_text="Lorem dolor")
     test_char = models.CharField(max_length=50, help_text="Lorem dolor")
-    test_bool = models.BooleanField(help_text="Lorem dolor")
+    test_bool = models.BooleanField(help_text="Lorem dolor", default=False)
     test_time = models.TimeField(help_text="Lorem dolor")
     test_slug = models.SlugField(help_text="Lorem dolor")
     test_text = models.TextField(help_text="Lorem dolor")
@@ -46,7 +46,7 @@ class TestThat(models.Model):
     test_url = models.URLField(help_text="Lorem dolor")
     test_int = models.IntegerField(help_text="Lorem dolor")
     test_date = models.DateField(help_text="Lorem dolor")
-    test_bool = models.BooleanField(help_text="Lorem dolor")
+    test_bool = models.BooleanField(help_text="Lorem dolor", default=True)
 
     class Meta:
         verbose_name = u'Test that'
