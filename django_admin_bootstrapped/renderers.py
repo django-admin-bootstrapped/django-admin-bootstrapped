f03f06c3e6011c46bd54abf2cf68337546bd9b9c
Fernando Gonzalez <efg@umail.ucsb.edu>
2015-01-23 02:16:02 -0800

    bootstrap3.html was moved to bootstrap3.utils

--- django_admin_bootstrapped/renderers.py
+++ django_admin_bootstrapped/renderers.py
@@ -6,7 +6,7 @@ from django.contrib.admin.widgets import (AdminDateWidget, AdminTimeWidget,
 from django.forms import (FileInput, CheckboxInput, RadioSelect, CheckboxSelectMultiple)
 
 from bootstrap3 import renderers
-from bootstrap3.html import add_css_class
+from bootstrap3.utils import add_css_class
 from bootstrap3.text import text_value
 
 class BootstrapFieldRenderer(renderers.FieldRenderer):
