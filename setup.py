ea2a710ff08a728416724d6c552b3abd80857e40
Riccardo Magliocchetti <riccardo.magliocchetti@gmail.com>
2015-03-13 23:40:18 +0100

    Bump release to 2.3.5

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     maintainer="Riccardo Magliocchetti",
     maintainer_email="riccardo.magliocchetti@gmail.com",
     name='django-admin-bootstrapped',
-    version='2.3.4',
+    version='2.3.5',
     description='A Bootstrap theme for Django Admin',
     long_description=open(os.path.join(os.path.dirname(__file__), 'README.rst')).read(),
     url='https://github.com/django-admin-bootstrapped/django-admin-bootstrapped',
